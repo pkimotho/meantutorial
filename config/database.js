0aa8e84ed205d927273ccc50d641486669ebe6de
Kimotho <plkimotho@gmail.com>
2019-05-23 15:18:32 +0300

    adds database name to the path link

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 crypto.randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/' + this.db,
+    uri: 'mongodb://localhost:27017/tutorialacosta',
     secret: crypto,
     db: 'tutorialacosta'
 };
